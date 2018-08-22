594d02f7cef50512bf085fa090607c016567724a
Zimniros <zimniros@protonmail.com>
2018-08-22 18:06:27 +0300

    Changes to UI.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,15 @@
 var navToogle = document.getElementById("toogle");
+var header = document.getElementById("header");
+var headerHeight = header.offsetHeight;
 
 navToogle.addEventListener("click", function() {
   document.body.classList.toggle("nav-is-open");
+});
+
+window.addEventListener("scroll", function() {
+  if (window.scrollY >= headerHeight) {
+    document.body.classList.add("sticky-header");
+  } else {
+    document.body.classList.remove("sticky-header");
+  }
 });
